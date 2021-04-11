58931d0108c07182bc6173abd9785a8bcd85c6f0
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-04-11 23:52:15 +0530

    Added: Code for adding contact detail in the AddressBook.

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ package AddressBookSystem;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program AddressBookMain class on Master Branch");
+		System.out.println("Create your first contact.");
+		ContactDetail contactBook = new ContactDetail("Nishi","Gautam","Mayur Vistar","Lucknow","UP",2260,"9553470596","nishi@gmail.com");
+		contactBook.printAddressbook();
 	}
 }
