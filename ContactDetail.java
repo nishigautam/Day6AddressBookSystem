fd1ba7ded8f5aefefdd6ed189facf616d30bc442
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-04-13 20:03:21 +0530

    Added: Ability to edit existing contact person

--- ContactDetail.java
+++ ContactDetail.java
@@ -1,5 +1,4 @@
 package AddressBookSystem;
-import java.util.Scanner;
 
 public class ContactDetail {
 	public String firstname;
@@ -8,11 +7,11 @@ public class ContactDetail {
 	public String state;
 	public String city;
 	public int pincode;
-	public String phonenumber;
+	public long phonenumber;
 	public String emailid;
 	
 	public ContactDetail(String firstname, String lastname, String address, 
-			String state, String city, int pincode, String phonenumber, String emailid) {
+			String state, String city, int pincode, long phonenumber, String emailid) {
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.address = address;
@@ -22,38 +21,75 @@ public class ContactDetail {
 		this.phonenumber = phonenumber;
 		this.emailid = emailid;
 	}
-
-	public void printAddressbook() {
-		System.out.println("First Name:" + this.firstname);
-		System.out.println("Last Name:" + this.lastname);
-		System.out.println("Address:" + this.address);
-		System.out.println("State:" + this.state);
-		System.out.println("City:" + this.city);
-		System.out.println("Pincode:" + this.pincode);
-		System.out.println("Phone Number:" + this.phonenumber);
-		System.out.println("Email:" + this.emailid);
+	
+	public String getFirstName() {
+		return firstname;
 	}
 	
-	public void addNewContact() {
-		Scanner addContact = new Scanner(System.in);
-		System.out.println("Add New Contact :");
-		System.out.println("First Name :");
-		String firstname = addContact.nextLine();
-		System.out.println("Last Name :");
-		String lastname = addContact.nextLine();
-		System.out.println("Address :");
-		String address = addContact.nextLine();
-		System.out.println("City:");
-		String city = addContact.nextLine();
-		System.out.println("State :");
-		String state = addContact.nextLine();
-		System.out.println("Pincode :");
-		int pincode = addContact.nextInt();
-		System.out.println("Phone Number :");
-		String phonenumber = addContact.nextLine();
-		System.out.println("Email Id :");
-		String emailid = addContact.nextLine();
-		System.out.println("First Name:" + firstname + "\nLast Name:" + lastname + "\nAddress:" + address + "\nCity:" + city + "\nState:" + state + "\nPin Code:" + pincode + "\nPhone Number:" + phonenumber + "\nEmail-ID:" + emailid);
-		addContact.close();
+	public void setFirstName(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastName() {
+		return lastname;
+	}
+	
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String setAddress(String address) {
+		return this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public int getPincode() {
+		return pincode;
+	}
+	
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	
+	public long getPhoneNumber() {
+		return phonenumber; 
+	}
+	
+	public void setPhoneNumber(long phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	
+	public String getEmailId() {
+		return emailid;
+	}
+	
+	public String setEmailId(String emailid) {
+		return this.emailid = emailid;
+	}
+	
+	@Override
+	public String toString() {
+		return "AddressBook [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address +
+				", city=" + city + ", state=" + state + ", pincode=" + pincode + ", phonenumber=" + phonenumber +
+				", emailId=" + emailid + "]";
 	}
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,83 @@
 package AddressBookSystem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Create your first contact.");
-		ContactDetail contactBook = new ContactDetail("Nishi","Gautam","Mayur Vistar","Lucknow","UP",2260,"9553470596","nishi@gmail.com");
-		contactBook.printAddressbook();
-		
-		ContactDetail newContact = new ContactDetail(null,null,null,null,null,0,null,null);
-		newContact.addNewContact();
+	
+	Scanner sc = new Scanner(System.in);
+	public ArrayList<ContactDetail> contactArray;
+	public Map<String,ContactDetail> contactMap;
+	public Main() {
+		contactArray = new ArrayList<>();
+		contactMap = new HashMap<>();
+	}
+	
+	public void addNewContact(String firstname, String lastname, 
+			String address, String city, String state, int pincode, 
+			long phonenumber, String emailid) {
+		ContactDetail con = new ContactDetail(firstname, lastname, address, city,
+				state, pincode, phonenumber, emailid);
+		contactArray.add(con);
+		contactMap.put(firstname, con);
+	}
+	
+	public void editContact() {
+		System.out.println("Enter the first name whose contact details is to be edited: ");
+		String fn = sc.nextLine();
+		ContactDetail obj = contactMap.get(fn);
+		System.out.println();
+		System.out.println("Enter the Address :");
+		String add=sc.nextLine();
+		obj.setAddress(add);
+		System.out.println("Enter the City :");
+		String citi = sc.nextLine();
+		obj.setCity(citi);
+		System.out.println("Enter State :");
+		String stat = sc.nextLine();
+		obj.setState(stat);
+		System.out.println("Enter Pincode :");
+		int pin = sc.nextInt();
+		obj.setPincode(pin);
+		System.out.println("Enter the PhoneNumber :");
+		long num = sc.nextInt();
+		obj.setPhoneNumber(num);
+		System.out.println("Enter the EmailID :");
+		String email = sc.nextLine();
+		obj.setEmailId(email);
+		System.out.println();
+	}
+	
+	public void printContact() {
+		System.out.println(contactArray);
+	}
+	
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter First Name :");
+		String firstname = s.nextLine();
+		System.out.println("Enter Last Name :");
+		String lastname = s.nextLine();
+		System.out.println("Enter Address :");
+		String address = s.nextLine();
+		System.out.println("City:");
+		String city = s.nextLine();
+		System.out.println("State :");
+		String state = s.nextLine();
+		System.out.println("Pincode :");
+		int pincode = s.nextInt();
+		System.out.println("Phone Number :");
+		long phonenumber = s.nextLong();
+		System.out.println("Email Id :");
+		String emailid = s.nextLine();
+		Main ContactOne = new Main();
+		ContactOne.addNewContact(firstname, lastname, address, city,
+				state, pincode, phonenumber, emailid);
+	ContactOne.printContact();
+	ContactOne.editContact();
+	System.out.println("After ContactBook gets edited :");
+	ContactOne.printContact();
 	}
 }
